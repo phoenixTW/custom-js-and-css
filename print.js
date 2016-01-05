5891270a9851d354c6ac6d9c7f353f411a3c6814
ArchanaChillala <archanac@thoughtworks.com>
2016-01-05 12:50:31 +0530

    [Kaustav/Archana] - #99 - Replaced drop down with Selected option for Data Set as text

--- print.js
+++ print.js
@@ -4,17 +4,31 @@ $(document).ready(function() {
 	var createFriendlyPeriod = function () {
 		var periodSection = $("div:contains('Period')").closest(".selectionBoxRow");
 		var periodInputBox = "<input id='periodInputBox' type='text'>";
+	
 		periodSection.append(periodInputBox);
+	}
+
+	var createFriendlyDataSet = function () {
+		var dataSetLabel = $("div:contains('Data Set')").closest(".selectionBoxRow");
+		var dataSetSelectedOption = $("#selectedDataSetId option:selected").text();
+		var printFriendlyDataSetOption = "<span id='datasetOption'>" + dataSetSelectedOption + "</span>"
+		
+		dataSetLabel.append(printFriendlyDataSetOption);
 	}
 
 	var resetPeriod = function () {
 		$("#periodInputBox").remove();
 	}
 
+	var resetDataSet = function () {
+		$("#datasetOption").remove();
+	}
+
 	$("#printButton").click(function(e) {
 		e.stopPropagation();
 		e.preventDefault();
 
+		createFriendlyDataSet();
 		createFriendlyPeriod();
 
 		$("span:contains('Value')").closest("tr").hide();
@@ -22,6 +36,7 @@ $(document).ready(function() {
 		window.print();
 
 		$("span:contains('Value')").closest("tr").show();
+		resetDataSet();
 		resetPeriod();
 	});
 });

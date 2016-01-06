a4b4e8c8534c9991db04bc7cd40869537f095360
ArchanaChillala <archanac@thoughtworks.com>
2016-01-06 12:51:48 +0530

    [Kaustav/Archana] - #99 - Added custom period and  User Name for print friendly form in data Entry

--- print.js
+++ print.js
@@ -1,11 +1,21 @@
+var snippet = {
+	getHeaderUserName: function () {
+		var userNameSection = "<div class='selectionLabel'>Data Entry User Name</div> <input class='customTextBox' type='textbox'>";
+		return "<div id='userNameRow' class='selectionBoxRow'>" + userNameSection + "</div>";
+	},
+
+	getHeaderPrintFriendlyPeriod: function () {
+		return "<div id='dateSection' class='selectionBoxRow'><div class='selectionLabel'>Date/s</div> <input class='customTextBox' type='textbox'> </div>";
+	}
+}
+
 $(document).ready(function() {
 	$("#printButton").removeAttr("onclick");
 
 	var createFriendlyPeriod = function () {
-		var periodSection = $("div:contains('Period')").closest(".selectionBoxRow");
-		var periodInputBox = "<input id='periodInputBox' type='text'>";
-	
-		periodSection.append(periodInputBox);
+		var periodSection = $(".selectionBoxRow:contains('Period')");
+		periodSection.hide();
+		$("#selectionBox").append(snippet.getHeaderPrintFriendlyPeriod());
 	}
 
 	/*var createFriendlyDataSet = function () {
@@ -31,14 +41,24 @@ $(document).ready(function() {
 
 		dataSetLabel.append(printFriendlyDataSet);
 	}
+	var dataEntryUserName = function() {
+		$("#selectionBox").append(snippet.getHeaderUserName());
+	}
 
 	var resetPeriod = function () {
-		$("#periodInputBox").remove();
+		$("#dateSection").remove();
+		$(".selectionBoxRow:contains('Period')").show();
 	}
 
 	var resetDataSet = function () {
 		$("#dataSetCheckBoxes").remove();
 	}
+
+	var resetUserNameRow = function () {
+		$("#userNameRow").remove();
+	}
+
+
 
 	$("#printButton").click(function(e) {
 		e.stopPropagation();
@@ -47,6 +67,7 @@ $(document).ready(function() {
 		checkBoxDataSet();
 		//createFriendlyDataSet();
 		createFriendlyPeriod();
+		dataEntryUserName();
 
 		$("span:contains('Value')").closest("tr").hide();
 
@@ -55,5 +76,6 @@ $(document).ready(function() {
 		$("span:contains('Value')").closest("tr").show();
 		resetDataSet();
 		resetPeriod();
+		resetUserNameRow();
 	});
 });
